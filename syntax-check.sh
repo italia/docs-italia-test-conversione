f037d9f84f224aa475dbb34d751bc99275699c25
danse <f.occhipinti@gmail.com>
2018-07-18 20:59:23 +0200

    aggiungi un contatore per il totale degli errori

--- syntax-check.sh
+++ syntax-check.sh
@@ -4,6 +4,8 @@ lt risultato-conversione/*/*/documento.rst | while read f
 do
     rst2html "$f" > /dev/null 2>> $e
 done
-cut -d':' -f1,3 < $e | sort | uniq -c
+cut -d':' -f1,3 < $e | sort | uniq -c | sort -n
+echo -n "total errors: "
+wc -l < $e
 echo "errors details in $e"
 
